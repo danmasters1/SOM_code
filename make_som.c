8862509be218b7f6a2c04ae7cc1262053dc0f010
Dan Masters <dmasters@ipac.caltech.edu>
2014-06-27 12:42:23 -0700

    Getting started

---
+++ make_som.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <math.h>
+
+int main(int argc, char *argv[]){
+
+  if(argc != 4) {
+    fprintf(stderr,"Use: %s nThreads input_data output \n", argv[0]);
+    return -1;
+  }
+
+
+}
+
+
