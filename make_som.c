59aa5cece8ecfc8ce4692191edcf25631ad4b0b5
Dan Masters <dmasters@ipac.caltech.edu>
2014-07-17 14:16:42 -0700

    Rectangular SOM with outputs for analysis/plotting, v1.

--- make_som.c
+++ make_som.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <math.h>
+#include <time.h>
 #define MAXLINE 4096
 
 #define ELEM_SWAP(a,b) { register double t=(a);(a)=(b);(b)=t; }
@@ -83,19 +84,19 @@ double randn (double mu, double sigma){
 }
 
 double learningFunction (double iter, double Niterations){
-  //Return the current value of learning rate function a(t)
+  // Return the current value of learning rate function a(t)
   return pow(0.01, (iter/Niterations));
 }
 
-double sigmaFunction (double iter, double Niterations, int gridside){
-  //Return the value of sigma for the neighborhood function at this time
+double sigmaFunction (double iter, double Niterations, double sigma_start){
+  // Return the value of sigma for the neighborhood function at this time
   double result;
-  result = (double) gridside * pow((1./(double)gridside),(iter/Niterations));
+  result = sigma_start * pow((1./sigma_start),(iter/Niterations));
   return result;
 }
 
 double neighborhoodFunction (double x1, double y1, double x2, double y2, double iter, double Niterations, double sigma){
-  //Return neighborhood learning value at this iteration
+  // Return neighborhood learning value at this iteration
   double sqdist;
   sqdist = pow((x1 - x2),2.) + pow((y1 - y2),2.);
   return exp(-sqdist/(sigma*sigma));
@@ -121,6 +122,7 @@ return sqrt(total/(double)n_elements);
 }
 
 double calcSeparation(double *vec, double *cell, int Ndimensions){
+  // Return squared Euclidean distance between two vectors
   int i=0;
   double dist = 0.;
   for(i=0;i<Ndimensions;i++){
@@ -133,17 +135,23 @@ double calcSeparation(double *vec, double *cell, int Ndimensions){
 int main(int argc, char *argv[]){
   // SOM implementation, on a rectangular grid.
 
-  FILE *f_data, *f_test;
+  FILE *f_data, *f_test, *f_out;
   double *fwhm, *ra, *dec, *i_auto, *redshift, *ebv, *Nf, *u, *g, *r, *iband, *z, *yg, *Y, *J, *H;
   int Ninput, *sed, *rlaw;
   long *ID;
-  int gridside = 100; //size of rectangular grid on a side
-  int Ncells = gridside*gridside;
+  int xgridside = 10; //size of x dimension of map
+  int ygridside = 20; 
+  int Ncells = xgridside*ygridside;
+  double sigma_start = fminf((double)xgridside, (double)ygridside);
+  fprintf(stderr,"sigma_start is %lf\n",sigma_start);
   int Ndimensions = 8;
-  int i = 0, j = 0;
-  double iter = 0, Niterations = 100; 
+  int i = 0, j = 0, k = 0;
+  double iter = 0, Niterations = 100, elapsed; 
+  clock_t start, end;
 
   char line[MAXLINE], cmd[MAXLINE];
+
+  start = clock();
 
   if(argc != 4) {
     fprintf(stderr,"Use: %s nThreads input_data output \n", argv[0]);
@@ -222,7 +230,7 @@ int main(int argc, char *argv[]){
     }
   }
 
-  double *ug, *gr, *ri, *iz, *zyg, *ygY, *YJ, *JH;
+  double *ug, *gr, *ri, *iz, *zyg, *ygY, *YJ, *JH, *redshift_good;
 
   ug = malloc(sizeof(double)*ngood);
   gr = malloc(sizeof(double)*ngood);
@@ -232,6 +240,7 @@ int main(int argc, char *argv[]){
   ygY = malloc(sizeof(double)*ngood);
   YJ = malloc(sizeof(double)*ngood);
   JH = malloc(sizeof(double)*ngood);
+  redshift_good = malloc(sizeof(double)*ngood);
   
   int idx = 0;
   for(i=0;i<Ninput;i++){
@@ -244,6 +253,7 @@ int main(int argc, char *argv[]){
       ygY[idx] = -2.5*(log10(yg[i]) - log10(Y[i]));
       YJ[idx] = -2.5*(log10(Y[i]) - log10(J[i]));
       JH[idx] = -2.5*(log10(J[i]) - log10(H[i]));
+      redshift_good[idx] = redshift[i];
       idx++;
     }
   }    
@@ -299,17 +309,18 @@ int main(int argc, char *argv[]){
   // Scale the data using the median and standard deviation
   for(i=0;i<ngood;i++){
     ug_scaled[i] = (ug[i]-med_ug)/stddev_ug;
-    gr_scaled[i] = (gr[i]-med_ug)/stddev_gr;
-    ri_scaled[i] = (ri[i]-med_ug)/stddev_ri;
-    iz_scaled[i] = (iz[i]-med_ug)/stddev_iz;
-    zyg_scaled[i] = (zyg[i]-med_ug)/stddev_zyg;
-    ygY_scaled[i] = (ygY[i]-med_ug)/stddev_ygY;
-    YJ_scaled[i] = (YJ[i]-med_ug)/stddev_YJ;
-    JH_scaled[i] = (JH[i]-med_ug)/stddev_JH;
+    gr_scaled[i] = (gr[i]-med_gr)/stddev_gr;
+    ri_scaled[i] = (ri[i]-med_ri)/stddev_ri;
+    iz_scaled[i] = (iz[i]-med_iz)/stddev_iz;
+    zyg_scaled[i] = (zyg[i]-med_zyg)/stddev_zyg;
+    ygY_scaled[i] = (ygY[i]-med_ygY)/stddev_ygY;
+    YJ_scaled[i] = (YJ[i]-med_YJ)/stddev_YJ;
+    JH_scaled[i] = (JH[i]-med_JH)/stddev_JH;
   }
 
   //---------------------------------------------------------------------------------
   //Define and initialize the map
+
   struct SOM {
     double x, y; 
     double cellweights[Ndimensions]; 
@@ -321,8 +332,8 @@ int main(int argc, char *argv[]){
   
   //Initialize cell weights with normally distributed random numbers and assign cell positions
   for(i=0;i<Ncells;i++){
-    mySOM[i].x = i % gridside;
-    mySOM[i].y = floor((double) i / (double) gridside);
+    mySOM[i].x = i % xgridside;
+    mySOM[i].y = floor((double) i / (double) xgridside);
     for(j=0;j<Ndimensions;j++){			
       mySOM[i].cellweights[j] = randn(0,1);
     }
@@ -334,8 +345,7 @@ int main(int argc, char *argv[]){
   int randomIdx, idx_bmu;
   double sep, minsep, a_t, H_t, sigma_t, sigma_t_squared, trainingVector[Ndimensions];
 
-  Niterations = 100000;
-
+  Niterations = 10000;
   for(iter=0;iter<Niterations;iter++){
     // Select at random from the training data
     // May want to change this to shuffling etc.
@@ -359,15 +369,15 @@ int main(int argc, char *argv[]){
 	idx_bmu = j;
       }      
     }
-    //fprintf(stderr,"This untrained minsep %lf\n",minsep);
     
-    // We have the BMU, now update the map
+    // We found the best matching unit, now update the map
 
     // Calculate a(t)
     a_t = learningFunction(iter,Niterations);
-    sigma_t = sigmaFunction(iter,Niterations,gridside);
+    sigma_t = sigmaFunction(iter,Niterations,sigma_start);
     sigma_t_squared = sigma_t * sigma_t;
-    
+ 
+    // Loop through cells for update, only look at ones reasonably close to BMU
 
     for(j=0;j<Ncells;j++){
 
@@ -382,20 +392,29 @@ int main(int argc, char *argv[]){
 	}
       } // end if cell is worth updating
     } // end loop over cells            
+
   } //end loop over iterations
 
-
   //---------------------------------------------------------------------------------
-  // Calculate the quantization error in the map
+  // Define final map and compute its properties
 
   double mean_err, median_err, quantization_errors[ngood];
 
-  struct SOM finalSOM[Ncells]; 
- 
-  /*
+  struct finalSOM {
+    int cell_index;
+    double x, y; 
+    double cellweights[Ndimensions];
+    double cell_occupation; // total number of objects in training sample associating with this cell
+    double U_value; // measure of separation from neighbor cells
+    double cell_dispersion; // how similar are the objects that associate with this cell
+    double cell_redshift; // average redshift of galaxies in cell, for use on training for now
+  };
+  struct finalSOM finalSOM[Ncells]; 
+  
   for(i=0;i<Ncells;i++){
-    finalSOM[i].x = i % gridside;
-    finalSOM[i].y = floor((double) i / (double) gridside);		
+    finalSOM[i].cell_index = i;
+    finalSOM[i].x = i % xgridside;
+    finalSOM[i].y = floor((double) i / (double) xgridside);		
     finalSOM[i].cellweights[0] = mySOM[i].cellweights[0]*stddev_ug + med_ug;
     finalSOM[i].cellweights[1] = mySOM[i].cellweights[1]*stddev_gr + med_gr;
     finalSOM[i].cellweights[2] = mySOM[i].cellweights[2]*stddev_ri + med_ri;
@@ -406,6 +425,7 @@ int main(int argc, char *argv[]){
     finalSOM[i].cellweights[7] = mySOM[i].cellweights[7]*stddev_JH + med_JH;
   }
   
+  // Calculate the occupation of each cell and average redshift of galaxies in each cell
   for(i=0;i<ngood;i++){
     // Compute the quantization error of the map in the original (unscaled) data space
     trainingVector[0] = ug[i];
@@ -426,40 +446,131 @@ int main(int argc, char *argv[]){
 	minsep = sep;
 	idx_bmu = j;
       }      
+    } 
+    // Update the occupation of the winning cell
+    finalSOM[idx_bmu].cell_occupation += 1;
+    finalSOM[idx_bmu].cell_redshift += redshift_good[i];
+  } 
+
+  for(i=0;i<Ncells;i++){
+    if(finalSOM[i].cell_occupation != 0.){
+      finalSOM[i].cell_redshift /= finalSOM[i].cell_occupation;
     }
-    quantization_errors[i] = sqrt(minsep);
   }
-  */
+
+  // Compute cell U-matrix values, using largest separation with neighboring cell (Kraaijveld)
+  double minx, maxx, miny, maxy, max_sep;
+  int tempidx;
   
+  for(i=0;i<Ncells;i++){
+    max_sep = 0;
+    minx = finalSOM[i].x - 1;
+    if(minx < 0){minx = 0;}
+    maxx = finalSOM[i].x + 1;
+    if(maxx == xgridside){maxx = xgridside-1;}
+    miny = finalSOM[i].y - 1;
+    if(miny < 0){miny = 0;}
+    maxy = finalSOM[i].y + 1;   
+    if(maxy == ygridside){maxy = ygridside-1;}
+
+    //fprintf(stderr,"This minx is %lf\n", minx);
+    //fprintf(stderr,"This maxx is %lf\n", maxx);
+    //fprintf(stderr,"This miny is %lf\n", miny);
+    //fprintf(stderr,"This maxy is %lf\n", maxy);
+
+    sep = 0;
+    max_sep = 0;
+    for(j=minx;j<=maxx;j++){
+      for(k=miny;k<=maxy;k++){
+	tempidx = k*xgridside + j;
+	//fprintf(stderr,"This j is %d\n", j);
+	//fprintf(stderr,"This k is %d\n", k);
+	//fprintf(stderr,"This tempidx is %d\n", tempidx);	
+	//fprintf(stderr,"This finalSOM[tempidx].x is %lf\n", finalSOM[tempidx].x);	
+	//fprintf(stderr,"This finalSOM[tempidx].y is %lf\n", finalSOM[tempidx].y);
+	sep = calcSeparation(finalSOM[tempidx].cellweights, finalSOM[i].cellweights, Ndimensions);
+	//fprintf(stderr, "This separation is %lf\n", sqrt(sep));
+	if(sep > max_sep){max_sep = sep;}
+      }
+    } // end loop over neighbors
+    
+    finalSOM[i].U_value = sqrt(max_sep);
+  } // end loop over map cells 
+
+  // Error checking, print average U value
+  sep = 0;
+  for(i=0;i<Ncells;i++){
+    sep += finalSOM[i].U_value;
+  }
+  fprintf(stderr,"Average U value is %lf\n", sep/Ncells);
+  
+
+  //---------------------------------------------------------------------------------
+  // Calculate the quantization error in the map
+
+  //double total_error=0;
   for(i=0;i<ngood;i++){
     // Compute the quantization error of the map in the original (unscaled) data space
-    trainingVector[0] = ug_scaled[i];
-    trainingVector[1] = gr_scaled[i];
-    trainingVector[2] = ri_scaled[i];
-    trainingVector[3] = iz_scaled[i];
-    trainingVector[4] = zyg_scaled[i];
-    trainingVector[5] = ygY_scaled[i];
-    trainingVector[6] = YJ_scaled[i];
-    trainingVector[7] = JH_scaled[i];
+    trainingVector[0] = ug[i];
+    trainingVector[1] = gr[i];
+    trainingVector[2] = ri[i];
+    trainingVector[3] = iz[i];
+    trainingVector[4] = zyg[i];
+    trainingVector[5] = ygY[i];
+    trainingVector[6] = YJ[i];
+    trainingVector[7] = JH[i];
 
     // Find the best-matching unit. 
+
     idx_bmu = 0;
-    minsep = calcSeparation(trainingVector,mySOM[0].cellweights,Ndimensions);
+    minsep = calcSeparation(trainingVector,finalSOM[0].cellweights,Ndimensions);
     for(j=1;j<Ncells;j++){
-      sep = calcSeparation(trainingVector,mySOM[j].cellweights,Ndimensions); //return squared Euclidean distance
+      sep = calcSeparation(trainingVector,finalSOM[j].cellweights,Ndimensions); //return squared Euclidean distance
       if (sep < minsep){
 	minsep = sep;
 	idx_bmu = j;
       }      
     }
     quantization_errors[i] = sqrt(minsep);
+    
+    /*
+    total_error=0;
+    //Let's check how well things match
+    fprintf(stderr,"New object\n");
+    for(j=0;j<Ndimensions;j++){
+      fprintf(stderr, "Training vector[j] = %lf, Cell weight is %lf\n",trainingVector[j],finalSOM[idx_bmu].cellweights[j]);
+      total_error += (trainingVector[j]-finalSOM[idx_bmu].cellweights[j])*(trainingVector[j]-finalSOM[idx_bmu].cellweights[j]);
+    }
+    fprintf(stderr,"Total error of %lf\n", sqrt(total_error));
+    */
+      
   }
 
   mean_err = calcMean(quantization_errors, ngood);
   median_err = quick_median(quantization_errors, ngood);
   fprintf(stderr,"The mean quantization error of this map is %lf\n",mean_err);
   fprintf(stderr,"The median quantization error of this map is %lf\n",median_err);
-   
+
+  
+  //---------------------------------------------------------------------------------
+  // Save the map to file
+  // Format: index x  y  weight1 weight2 ... weightN U_value cell_dispersion cell_redshift
+
+  f_out = fopen(argv[3],"w");
+  if(f_out == NULL){
+    fprintf(stderr,"Cannot open output file %s !\n",argv[3]);
+    exit(1);
+  }
+
+  for(i=0;i<Ncells;i++){
+    fprintf(f_out,"%d %f %f %f %f %f %f %f %f %f %f %f %f %f %f\n",finalSOM[i].cell_index,finalSOM[i].x,finalSOM[i].y,finalSOM[i],finalSOM[i].cellweights[0],finalSOM[i].cellweights[1],finalSOM[i].cellweights[2],finalSOM[i].cellweights[3],finalSOM[i].cellweights[4],finalSOM[i].cellweights[5],finalSOM[i].cellweights[6],finalSOM[i].cellweights[7], finalSOM[i].cell_occupation, finalSOM[i].U_value, finalSOM[i].cell_dispersion, finalSOM[i].cell_redshift);
+  }
+  fclose(f_out);
+
+  end = clock();
+  elapsed = ((double) (end - start)) / CLOCKS_PER_SEC;
+  fprintf(stderr,"It took %lf seconds.\n",elapsed); 
+
 } // End SOM program
 
 
